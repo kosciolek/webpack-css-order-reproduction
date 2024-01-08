9676e5c95ffb2389aab978c11b30fdeb33a06a70
kosciolek <45483493+kosciolek@users.noreply.github.com>
2024-01-08 15:16:25 +0100

    modules

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { pkg1 } from "pkg1";
 import first from "./first.module.css";
 import second from "./second.module.css";
 
-console.log(second.second);
 console.log(first.first);
+console.log(second.second);
+
 pkg1();
